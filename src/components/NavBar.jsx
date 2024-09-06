d32312aaf2c132d5ad629cbaf3927d64b9ace2b9
Raden Mohamad Adrian Ramadhan Hendar Wibawa <raden.mohamad11@ui.ac.id>
2024-09-06 21:38:51 +0700

    style: tweaked design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useTheme } from '../context/ThemeContext';
+import { GiSkullCrossedBones } from "react-icons/gi";
 
 const Navbar = () => {
   // State to manage the navbar's visibility
   const [nav, setNav] = useState(false);
   const { theme, toggleTheme } = useTheme();
+  const location = useLocation();
 
   // Toggle function to handle the navbar's display
   const handleNav = () => {
@@ -21,10 +23,10 @@ const Navbar = () => {
   ];
 
   return (
-    <div className='bg-green-500 dark:bg-black flex justify-between items-center h-24 mx-auto px-4 text-black dark:text-white '>
+    <div className='bg-green-600 dark:bg-black flex justify-between items-center h-24 mx-auto px-4 text-black dark:text-white '>
       {/* Logo */}
       <Link to="/">
-      <h1 className='w-full text-3xl font-bold text-black dark:text-[#00df9a]'>Raden Mohamad Adrian</h1>
+      <h1 className='w-full text-3xl font-bold text-white dark:text-[#00df9a] flex gap-3'><GiSkullCrossedBones />Raden Mohamad Adrian</h1>
       </Link>
 
       {/* Desktop Navigation */}
@@ -34,7 +36,9 @@ const Navbar = () => {
               <Link to={item.path}>
               <li
                   key={item.id}
-                  className='p-4 hover:bg-black dark:hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-white dark:hover:text-black'
+                  className={`p-4 hover:bg-black dark:hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-white dark:hover:text-black 
+                    ${location.pathname === item.path && 'bg-black dark:bg-[#00df9a] text-white dark:text-black'}
+                  `}
               >
                   {item.text}
               </li>
@@ -70,14 +74,16 @@ const Navbar = () => {
       >
         {/* Mobile Logo */}
         <Link to="/">
-        <h1 className='w-full text-3xl font-bold dark:text-[#00df9a] text-black m-4'>Raden Mohamad Adrian</h1>
+        <h1 className='w-full text-3xl font-bold dark:text-[#00df9a] text-white m-4 flex gap-3'><GiSkullCrossedBones />Raden Mohamad Adrian</h1>
         </Link>
         {/* Mobile Navigation Items */}
         {navItems.map(item => (
           <Link to={item.path}>
           <li
             key={item.id}
-            className='p-4 border-b rounded-xl hover:bg-black dark:hover:bg-[#00df9a] duration-300 hover:text-white dark:hover:text-black cursor-pointer border-gray-600'
+            className={`p-4 border-b rounded-xl hover:bg-black dark:hover:bg-[#00df9a] duration-300 hover:text-white dark:hover:text-black cursor-pointer border-gray-600
+              ${location.pathname === item.path && 'bg-black dark:bg-[#00df9a] text-white dark:text-black'}
+            `}
           >
             {item.text}
           </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
 // source: https://github.com/tailwindtoolbox/Profile-Card
 import React from "react";
 import { FaFileDownload, FaBriefcase } from "react-icons/fa";
+import { GiSkullCrossedBones } from "react-icons/gi";
 
 const Home = () => {
 	return (
-        <div className="bg-gradient-to-r from-white via-green-200 to-green-400 dark:bg-gradient-to-r dark:from-green-300 dark:via-green-600 dark:to-green-900 dark:bg-gradient-to-r dark:from-green-300 dark:via-green-600 dark:to-green-900">
+        <div className="bg-gradient-to-r from-white via-green-200 to-green-400 dark:bg-gradient-to-r dark:from-green-300 dark:via-green-600 dark:to-green-900 ">
 
         
         <div className="max-w-4xl flex items-center h-screen  flex-wrap mx-auto lg:my-0">
-    	<div id="profile" className="transform transition duration-500 hover:scale-125 w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white dark:bg-gray-900 opacity-75 mx-6 lg:mx-0">
+    	<div id="profile" className="mt-5 lg:mt-0 transform transition duration-500 lg:hover:scale-125 hover:scale-105 w-full lg:w-3/5 rounded-lg  shadow-2xl bg-white dark:bg-black opacity-75 mx-6 lg:mx-0">
 	
 
 		<div className="p-4 md:p-12 text-center lg:text-left">
-			<div className="block lg:hidden rounded-full mx-auto -mt-16 h-48 w-48 bg-cover bg-center hidden-profile-pic"></div>
+			<div className="block lg:hidden rounded-full mx-auto -mt-16 h-48 w-48 bg-cover bg-center hidden-profile-pic opacity-100"></div>
 			
 			<h1 className="dark:text-white text-3xl font-bold pt-8 lg:pt-0">Raden Mohamad Adrian</h1>
 			<div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
@@ -30,8 +31,8 @@ cybersecurity.</p>
 				
 			</div>
 
-			<div className="mt-6 pb-16 lg:pb-0 w-2/12 lg:w-full mx-auto flex flex-wrap items-center">
-				<a className="link" href="https://github.com/AdrianRamadhan27" target="_blank" data-tippy-content="@github_handle"><svg className="mr-10 h-6 fill-current text-gray-600 hover:text-green-700" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg></a>
+			<div className="mt-6 pb-16 lg:pb-0 justify-center lg:justify-start lg:w-full mx-auto flex flex-wrap items-center gap-10">
+				<a className="link" href="https://github.com/AdrianRamadhan27" target="_blank" data-tippy-content="@github_handle"><svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg></a>
 				<a className="link" href="https://www.linkedin.com/in/adrian-voiz/" target="_blank" data-tippy-content="@github_handle"><svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
             </div>
 			
@@ -41,7 +42,7 @@ cybersecurity.</p>
 	</div>
 	
 	<div className="w-full lg:w-2/5">
-		<img src={import.meta.env.BASE_URL+"images/profile.jpg"} className="transform transition duration-500 hover:scale-125 rounded-none lg:rounded-lg shadow-2xl hidden lg:block"/>
+		<img src={import.meta.env.BASE_URL+"images/profile.jpg"} className="transform transition duration-500 hover:scale-125 rounded-none lg:rounded-lg shadow-2xl hidden lg:block "/>
 	</div>
 	
 
