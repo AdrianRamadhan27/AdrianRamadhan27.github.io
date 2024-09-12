f69415e9d3ac4dbd7cfc1fa44728e5ced817382a
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2024-09-12 08:54:46 +0700

    fix: adjust tech stack mobile margins

--- src/components/layout/TechStacks.jsx
+++ src/components/layout/TechStacks.jsx
@@ -1,8 +1,8 @@
 import TechCard from "../ui/TechCard";
 export function TechStacks() {
     return (
-        <div className="bg-opacity-75 dark:bg-opacity-75 rounded-md shadow-lg p-3 flex flex-col bg-white dark:bg-black text-black dark:text-white gap-3 m-16 h-fit">
-            <h1 className="text-2xl font-bold text-center">My Expertise</h1>
+        <div className="bg-opacity-75 dark:bg-opacity-75 rounded-md shadow-lg p-3 flex flex-col bg-white dark:bg-black text-black dark:text-white gap-3 m-10 md:m-16 h-fit">
+            <h1 className="text-2xl font-bold text-center">Tech Stacks</h1>
             <div className="grid grid-rows-4 md:grid-rows-1 md:grid-cols-4">
                 <div className="flex flex-col gap-2 md:border-r-2 border-dashed px-2 py-2 md:py-0">
                     <h2 className="text-lg max-w-64 truncate">Programming Languages</h2>
